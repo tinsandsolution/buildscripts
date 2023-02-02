81dd4073a521a8fad6080c7cc2f4f7152edd0a6a
Lara Duong <100981273+tinsandsolution@users.noreply.github.com>
2023-02-01 22:33:11 -0700

    Create remote_desktop_ubuntu.sh

---
+++ remote_desktop_ubuntu.sh
@@ -0,0 +1,15 @@
+#On Vultr,
+
+#we can simply follow the instructions here.
+
+#https://www.vultr.com/docs/install-gui-environment-for-ubuntu/
+
+# This script is meant to automate the setting up of a gui environment
+
+# Step 1: Spin up an ubuntu server in vultr
+
+# Step 2: Log in as root
+
+# Step 3: git clone https://github.com/tinsandsolution/buildscripts.git
+
+# Step 4: bash buildscripts/remote_desktop_ubuntu.sh 
